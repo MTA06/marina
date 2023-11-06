b54a435e0533d157fe3548ae78fa49eab1f983a8
MTA06 <117911035+MTA06@users.noreply.github.com>
2023-11-06 09:01:01 -0300

    Add files via upload

---
+++ aluramidi-curso-aula4/main.js
@@ -0,0 +1,13 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+ const listaDeTeclas = document.querySelectorAll ('tecla')
+  let contador = 0;
+while (contador<listaDeElemento.length){
+listaDeElemento [contador].onclick= function () {
+        tocaSom(idAudio);
+    }
+
+} 
